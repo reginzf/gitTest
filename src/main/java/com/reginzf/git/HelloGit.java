57fc3d6de9a25b128913db95e0b9fd3aa52f5683
reginzf <reginzf@163.com>
2020-10-04 20:58:39 +0800

    在线更新

--- src/main/java/com/reginzf/git/HelloGit.java
+++ src/main/java/com/reginzf/git/HelloGit.java
@@ -3,8 +3,9 @@ package com.reginzf.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!");
-		System.out.println("更新1");
-		System.out.println("创建分支");
-		System.out.println("主线添加");
+		System.out.println("赂眉脨脗1");
+		System.out.println("麓麓陆篓路脰脰搂");
+		System.out.println("脰梅脧脽脤铆录脫");
+		System.out.println("鍦ㄧ嚎娣诲姞")
 	}
 }
